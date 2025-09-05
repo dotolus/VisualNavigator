a175985ed3ca9723680e683ef2fa013dbb0572c5
Dotan <dotolus@users.noreply.github.com>
2025-09-05 09:23:22 +0300

    Add files via upload

---
+++ graph-data.js
@@ -0,0 +1,135 @@
+const data = {
+  id: "t-space",
+  type: "step",
+  name: "פרויקט על",
+  url: "https://drive.google.com/drive/folders/1",
+  children: [
+    {
+      id: "content",
+      type: "folder",
+      name: "תכני ליווי",
+      url: "https://drive.google.com/drive/folders/17Hq",
+      children: [
+        {
+          id: "final",
+          type: "folder",
+          name: "תוצרים מוגמרים",
+          url: "https://drive.google.com/drive/folders/1f",
+          children: [
+            {
+              id: "final-nebulae",
+              type: "folder",
+              name: "שלב 1",
+              url: "https://drive.google.com/drive/folders/1xC",
+              children: [
+                {
+                  id: "my-roadmap",
+                  type: "doc",
+                  name: "תכנון",
+                  url: "https://docs.google.com/document/dd",
+                  long:"תכנון מלא"
+                },
+                {
+                  id: "schedule-management",
+                  type: "doc",
+                  name: "לוז",
+                  url: "https://docs.google.com/document/d/1.0",
+                },
+                
+              ]
+            }
+          ]
+        },
+        {
+          id: "prep",
+          type: "folder",
+          name: "חומרים",
+          url: "https://drive.google.com/drive/folders/M"
+        },
+        {
+          id: "guides",
+          type: "folder",
+          name: "חוברות",
+          url: "https://drive.google.com/drive/folders/sE",
+          children: [
+            {
+              id: "personal",
+              type: "doc",
+              name: "פרויקט",
+              short: "מסמך",
+              long: "פרויקט אישי",
+              url: "https://docs.google.com/document/dqq"
+            },
+           
+          ]
+        },
+        {
+          id: "umbrela",
+          type: "folder",
+          name: "מטרייה",
+          short: "",
+          long: "",
+          url: ""
+        }
+      ]
+    },
+    {
+      id: "meetings",
+      type: "folder",
+      name: "סיכומי פגישות",
+      url: "https://drive.google.com/drive/folders/dd",
+       children: [
+            {
+              id: "aggressive-dev-plan",
+              type: "doc",
+              name: "תוכנית צמיחה",
+              short: "מסמך",
+              long: "",
+              url: ""
+            },
+            {
+              id: "bootstrap-plan",
+              type: "doc",
+              name: "פגישה",
+              short: "מסמך",
+              long: "",
+              url: ""
+            },
+            {
+              id: "critical-path-for-success",
+              type: "doc",
+              name: "נתיב קריטי",
+              short: "מסמך",
+              long: "",
+              url: ""
+            },
+            {
+              id: "all-meetings-summary",
+              type: "doc",
+              name: "מסמך סיכום פגישות מרוכז",
+              short: "מסמך",
+              long: "המסמך שמגדיר מה המשימות עד לפגישה האישית הבאה",
+              url: "https://docs.google.com/document/d/y"
+            },
+
+
+          ]
+    },
+    {
+      id: "reports",
+      type: "folder",
+      name: "מעקבים ודוחות",
+      url: "https://drive.google.com/drive/folders/",
+      children: [
+            {
+              id: "daily-report",
+              type: "doc",
+              name: "דוח יומי",
+              short: "מסמך",
+              long: "סיכום פעולות יומי",
+              url: "https://docs.google.com/spreadsheets/s"
+            }
+          ]
+    }
+  ]
+};
